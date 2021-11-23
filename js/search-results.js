eb870df642ca26a1357d3791ff23991144c95058
MikaChandia <mchandia@udesa.edu.ar>
2021-11-23 12:00:03 -0300

    ja de search-results(hay algo mal) + corrección buscador

--- js/search-results.js
+++ js/search-results.js
@@ -1,4 +1,11 @@
-let url = `https://api.themoviedb.org/3/search/movie?api_key=9356bdf8fcc476bc9886493a22d862eb&language=en-US&page=1&include_adult=false`;
+let qs= location.search; //obtengo la qs de la url
+let queryStringObj= new URLSearchParams(qs); //transformo lq query string en un objeto literal
+let resultado = queryStringObj.get('resultado'); //obtengo el dato id del objeto literal
+
+let mensajeBusqueda = document.querySelector('.leyendaResultados');
+mensajeBusqueda.innerText = `Resultados para "${resultado}"`
+
+let url = `https://api.themoviedb.org/3/search/movie?api_key=9356bdf8fcc476bc9886493a22d862eb&language=en-US&query=${resultado}`;
 
 fetch(url)
     .then(function(response){
@@ -7,7 +14,25 @@ fetch(url)
     .then(function(data){
        console.log(data);
 
+       let info = data.results;
+       let resultadosMovies = '';
+       let resultados = document.querySelector('.elementosEncontrados')
         
+       if (resultadosMovies.length == 0){
+           let sinResultados = decument.querySelector('.sinResultados')
+           sinResultados.innerText = `No se encontraron resultados de búsqueda para '${resultado}'`
+           
+       } else {
+           for (let i=0; i<5; i++){
+               resultadosMovies += `<article>
+                                    <img src="https://image.tmdb.org/t/p/original/${info[i].poster_path}.jpg">
+                                    <h2>${info[i].title}</h2>
+                                    </article>`           
+                                }
+                                
+            resultados.innerHTML = resultadosMovies;
+       }
+    
 
     })
     .catch(function(error){
