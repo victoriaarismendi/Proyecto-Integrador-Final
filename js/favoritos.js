fe8d60cff3dfc6fce3e1d76dc6e013c0fa64320c
victoriaarismendi <varismendi@udesa.edu.ar>
2021-11-23 17:23:36 -0300

    favoritos

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,22 +1,68 @@
-//favoritos peliculas
+//favoritos series y peliculas
 //recupero storage y transformo en array
+let recuperoStorage = localStorage.getItem ('favoritosSeries');
 let recuperoStorageS = localStorage.getItem ('favoritos');
+let favoritosSeries = JSON.parse (recuperoStorage);
 let favoritos = JSON.parse (recuperoStorageS);
+console.log(favoritosSeries);
 console.log(favoritos);
 
 //capturar el contenedor de los elementos a mostrar
+let sectionS = document.querySelector ('#series');
+let seriesFavoritas = '' //la rellenamos con cada vuelta del for, para poder tener los datos adentro
 let section = document.querySelector ('.peliculas');
-let peliculasFavoritas = '' //la rellenamos con cada vuelta del for, para poder tener los datos adentro
+let peliculasFavoritas = '' 
 
 //si el storage esta vacio indicamos al usuario que no hay favoritos seleccionados
+if (favoritosSeries == null || favoritosSeries.length ==0) {
+    sectionS.innerHTML= '<h1>No hay favoritos seleccionados </h1>'
+    sectionS.style.color = "red"
+} else {
+    //for para recorrer el array
+    for (let i=0; i<favoritosSeries.length; i++) {
+        buscarYMostrarFavoritosSeries (favoritosSeries[i]);
+    }
+}
+
+//if parte peliculas 
 if (favoritos == null || favoritos.length ==0) {
     section.innerHTML= '<h1>No hay favoritos seleccionados </h1>'
+    section.style.color = "red" 
 } else {
     //for para recorrer el array
     for (let i=0; i<favoritos.length; i++) {
         buscarYMostrarFavoritos (favoritos[i]);
     }
 }
+
+//series
+function buscarYMostrarFavoritosSeries(id) {
+    //fetch para buscar los elementos del array
+    let url = `https://api.themoviedb.org/3/tv/${id}?api_key=d3bf40c9b6ae8b0603c799bd0fc81e36` 
+
+    fetch(url)
+        .then (function (response) {
+            return response.json ()
+         })
+        .then (function (data) {
+            console.log(data);
+            seriesFavoritas += `<article class="pelicula">
+            <a href="detail-series.html?id=${ data.id }">
+                <img src="https://image.tmdb.org/t/p/w342/${ data.poster_path }" alt="The Duff">
+                <p>${ data.original_name }</p>
+                <p>${data.first_air_date}<p>
+            </a>
+        </article>
+        `
+        //mostrarlo al usuario
+            sectionS.innerHTML = seriesFavoritas;
+        })
+        .catch (function (error) {
+            console.log(error);
+        })
+}
+
+//peliculas
 function buscarYMostrarFavoritos(id) {
     //fetch para buscar los elementos del array
     let url =  `https://api.themoviedb.org/3/movie/${id}?api_key=d3bf40c9b6ae8b0603c799bd0fc81e36`
@@ -46,50 +92,6 @@ function buscarYMostrarFavoritos(id) {
 
 
 
-//favoritos series
-//recupero storage y transformo en array
-let recuperoStorage = localStorage.getItem ('favoritosSeries');
-let favoritosSeries = JSON.parse (recuperoStorage);
-console.log(favoritosSeries);
-
-//capturar el contenedor de los elementos a mostrar
-let sectionS = document.querySelector ('#series');
-let seriesFavoritas = '' //la rellenamos con cada vuelta del for, para poder tener los datos adentro
-
-//si el storage esta vacio indicamos al usuario que no hay favoritos seleccionados
-if (favoritosSeries == null || favoritosSeries.length ==0) {
-    sectionS.innerHTML= '<h1>No hay favoritos seleccionados </h1>'
-} else {
-    //for para recorrer el array
-    for (let i=0; i<favoritosSeries.length; i++) {
-        buscarYMostrarFavoritosSeries (favoritosSeries[i]);
-    }
-}
-function buscarYMostrarFavoritosSeries(id) {
-    //fetch para buscar los elementos del array
-    let url = `https://api.themoviedb.org/3/tv/${id}?api_key=d3bf40c9b6ae8b0603c799bd0fc81e36` 
-
-    fetch(url)
-        .then (function (response) {
-            return response.json ()
-         })
-        .then (function (data) {
-            console.log(data);
-            seriesFavoritas += `<article class="pelicula">
-            <a href="detail-series.html?id=${ data.id }">
-                <img src="https://image.tmdb.org/t/p/w342/${ data.poster_path }" alt="The Duff">
-                <p>${ data.original_name }</p>
-                <p>${data.first_air_date}<p>
-            </a>
-        </article>
-        `
-        //mostrarlo al usuario
-            sectionS.innerHTML = seriesFavoritas;
-        })
-        .catch (function (error) {
-            console.log(error);
-        })
-}
     
 console.log('validando');
 
